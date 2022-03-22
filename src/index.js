79813507db7259daa9dca5cc16d540274a58d273
Khalifa Abourawi <kabourawi@Khalifas-MBP.attlocal.net>
2022-03-21 21:01:15 -0400

    new site with js i think

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import react from "react"
+import reactDom from "react-dom"
+
+function App() {
+  const[count, setCount] = React.useState(0)
+
+  return (
+    <div>
+      Hello from React!
+      <button onClick={() => setCount(count + 1)}>Click me {count}</button>
+    </div>
+  )
+}
+ReactDOM.render(<App></App>, document.getElementById('app'))
